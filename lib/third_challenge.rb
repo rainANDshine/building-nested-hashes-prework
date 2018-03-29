5b9d5fccd1992eea5a189b9c0a17e336d5c5f414
Shun Yao <ysmiracle@hotmail.com>
2018-03-29 02:22:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
         },
       :matriarch => {
         #your key/value pairs here
